15a79bdca5b28cb178575638319164076b2f89b2
Артем Чаадаев <perfectgentleman.de@ya.ru>
2022-09-28 00:06:41 +0300

    passwords work

--- cmd/go-basic-auth-example/main.go
+++ cmd/go-basic-auth-example/main.go
@@ -2,12 +2,14 @@ package main
 
 import (
 	"context"
+	"crypto/sha1"
 	"encoding/json"
 	"fmt"
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/perfectgentlemande/go-basic-auth-example/internal/logger"
+	"golang.org/x/crypto/pbkdf2"
 )
 
 type APIError struct {
@@ -43,7 +45,8 @@ func (c *Controller) authorize(username, password string) error {
 		return fmt.Errorf("No user with username: %s", username)
 	}
 
-	if pass != password {
+	pwd := string(pbkdf2.Key([]byte(password), []byte(c.salt), 4096, 32, sha1.New))
+	if pass != pwd {
 		return fmt.Errorf("Wrong password for username: %s", username)
 	}
 
@@ -70,14 +73,18 @@ func (c *Controller) postVerify(w http.ResponseWriter, r *http.Request) {
 
 type Controller struct {
 	storedUsernamesWithPasswords map[string]string
+	salt                         string
 }
 
 func main() {
 	c := Controller{
 		storedUsernamesWithPasswords: map[string]string{
-			"tst_usr_01": "tst_pwd_01",
-			"tst_usr_02": "tst_pwd_02",
+			// tst_pwd_01
+			"tst_usr_01": string([]byte{211, 157, 140, 249, 4, 171, 115, 195, 58, 50, 239, 253, 122, 187, 63, 33, 118, 240, 190, 57, 208, 131, 251, 22, 150, 166, 203, 200, 27, 129, 107, 41}),
+			// tst_pwd_02
+			"tst_usr_02": string([]byte{204, 238, 205, 169, 0, 192, 149, 151, 193, 101, 104, 48, 194, 211, 21, 196, 113, 80, 198, 118, 15, 0, 145, 142, 136, 155, 113, 236, 204, 13, 202, 68}),
 		},
+		salt: "RKt1Q@6Es@vdrh.iyg.4OMuuiKwf)ui_rJ9-4*SW.(yY47(TjVWrVuf1Blw(OFdq",
 	}
 
 	r := chi.NewRouter()
--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.18
 require (
 	github.com/go-chi/chi/v5 v5.0.7
 	github.com/sirupsen/logrus v1.9.0
+	golang.org/x/crypto v0.0.0-20220926161630-eccd6366d1be
 )
 
 require golang.org/x/sys v0.0.0-20220715151400-c0bba94af5f8 // indirect
